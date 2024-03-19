684c1f532c30e0950f7a254898e0bc932d63ae75
Miguel Ureña <mureatencio@gmail.com>
2024-03-19 16:10:26 -0600

    Add logic to basic tableview prefetch

--- Pokedex/Pokedex/Sections/PokemonList/PokemonListViewController.swift
+++ Pokedex/Pokedex/Sections/PokemonList/PokemonListViewController.swift
@@ -6,29 +6,30 @@
 import UIKit
 
 class PokemonListViewController: UIViewController {
-	private let tableView = UITableView()
-	private let loadingIndicator = UIActivityIndicatorView()
+    private let tableView = UITableView()
+    private let loadingIndicator = UIActivityIndicatorView()
     
     lazy var pokemonListViewModel: PokemonListViewModel = {
        PokemonListViewModel()
     }()
 
-	private var showActivityIndicator: Bool = true {
-		didSet {
-			if showActivityIndicator {
-				loadingIndicator.startAnimating()
-			} else {
-				loadingIndicator.stopAnimating()
-			}
-		}
-	}
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		self.navigationItem.title = "List of Pokemons"
-		setupTableView()
-		setupLoadingIndicator()
+    private var showActivityIndicator: Bool = true {
+        didSet {
+            if showActivityIndicator {
+                loadingIndicator.startAnimating()
+            } else {
+                loadingIndicator.stopAnimating()
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title = "List of Pokemons"
+        setupTableView()
+        setupLoadingIndicator()
         initViewModel()
-	}
+    }
     
     func initViewModel() {
         pokemonListViewModel.reloadTableView = { [weak self] in
@@ -42,69 +43,69 @@ class PokemonListViewController: UIViewController {
         }
         pokemonListViewModel.getPokemonList()
     }
-	
-	private func setupLoadingIndicator() {
-		loadingIndicator.translatesAutoresizingMaskIntoConstraints = false
-		loadingIndicator.hidesWhenStopped = true
-		loadingIndicator.style = .medium
-		view.addSubview(loadingIndicator)
-		
-		NSLayoutConstraint.activate([
-			loadingIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-			loadingIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-		])
-	}
-	
-	private func setupTableView() {
-		tableView.separatorColor = .clear
-		tableView.translatesAutoresizingMaskIntoConstraints = false
-		view.addSubview(tableView)
-		
-		NSLayoutConstraint.activate([
-			tableView.topAnchor.constraint(equalTo: view.topAnchor),
-			tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-			tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-			tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
-		])
-		
-		tableView.dataSource = self
+    
+    private func setupLoadingIndicator() {
+        loadingIndicator.translatesAutoresizingMaskIntoConstraints = false
+        loadingIndicator.hidesWhenStopped = true
+        loadingIndicator.style = .medium
+        view.addSubview(loadingIndicator)
+        
+        NSLayoutConstraint.activate([
+            loadingIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            loadingIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        ])
+    }
+    
+    private func setupTableView() {
+        tableView.separatorColor = .clear
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        
+        tableView.dataSource = self
         tableView.delegate = self
-		tableView.rowHeight = UITableView.automaticDimension
-		tableView.estimatedRowHeight = 44.0
-		tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-	}
-	
-	private func showErrorAlert() {
-		let alert = UIAlertController(title: "Error", message: "Something went wrong", preferredStyle: .alert)
-		self.present(alert, animated: true, completion: nil)
-	}
-
+        tableView.prefetchDataSource = self
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 44.0
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    private func showErrorAlert() {
+        let alert = UIAlertController(title: "Error", message: "Something went wrong", preferredStyle: .alert)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    private func isLoadingCell(for indexPath: IndexPath) -> Bool {
+        return indexPath.row >= (pokemonListViewModel.pokemonViewModels.count - 10)
+    }
 }
 
-extension PokemonListViewController: UITableViewDataSource, UITableViewDelegate {
-	func numberOfSections(in tableView: UITableView) -> Int {
-		return 1
-	}
-	
-	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+extension PokemonListViewController: UITableViewDataSource, UITableViewDelegate, UITableViewDataSourcePrefetching {
+    
+    func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
+        print(indexPaths)
+        if indexPaths.contains(where: isLoadingCell) && !pokemonListViewModel.isFetchingData && !pokemonListViewModel.hasReachedEnd {
+            pokemonListViewModel.getPokemonList()
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return pokemonListViewModel.pokemonViewModels.count
-	}
-	
+    }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         cell.textLabel?.text = pokemonListViewModel.pokemonViewModels[indexPath.row].name.capitalized
         return cell
     }
-    
-    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        if indexPath.row == pokemonListViewModel.pokemonViewModels.count - 1 {
-            print("reach end")
-            // Execute reload
-            DispatchQueue.main.async {
-                self.showActivityIndicator = true
-            }
-            pokemonListViewModel.getPokemonList()
-        }
-    }
-    
 }
--- Pokedex/Pokedex/ViewModels/PokemonListViewModel.swift
+++ Pokedex/Pokedex/ViewModels/PokemonListViewModel.swift
@@ -13,6 +13,11 @@ class PokemonListViewModel {
         self.service = service
     }
     
+    var isFetchingData = false
+
+    var hasReachedEnd: Bool {
+        return reachEnd
+    }
     private var reachEnd = false
     
     var reloadTableView: (() -> Void)?
@@ -22,23 +27,27 @@ class PokemonListViewModel {
             reloadTableView?()
         }
     }
-    
+  
     func getPokemonList() {
-        if !reachEnd {
-            service.getPokemonCharacters(offset: pokemonViewModels.count) { result in
+        guard !isFetchingData && !reachEnd else { return }
+        isFetchingData = true
+        
+        service.getPokemonCharacters(offset: pokemonViewModels.count) { [weak self] result in
+            guard let self = self else { return }
+            DispatchQueue.main.async {
+                self.isFetchingData = false
                 switch result {
                 case .success(let pokemonResponse):
                     self.pokemonViewModels.append(contentsOf: pokemonResponse.results.map(PokemonViewModel.init))
                     self.reachEnd = pokemonResponse.next == nil
-                case .failure(let failure):
-                    print(failure)
-                    // TODO: Manage error - What to do here??
+                case .failure(let error):
+                    print(error)
+                    // TODO: Handle error, e.g., by showing an alert or a placeholder view
                 }
             }
         }
-        
     }
-    
+
 }
 
 struct PokemonViewModel {
